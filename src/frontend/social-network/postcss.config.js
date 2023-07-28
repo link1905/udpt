b1f12c6434a6f5219bf222b39f2efe7299dc83c0
pthienquan1 <quan01011997123@gmail.com>
2023-07-28 23:13:35 +0700

    update fix tailwind

--- src/frontend/social-network/postcss.config.js
+++ src/frontend/social-network/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    '@tailwindcss/jit': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
--- src/frontend/social-network/src/App.tsx
+++ src/frontend/social-network/src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-center text-red-700 text-lg sm:text-2xl md:text-3xl lg:text-4xl lg:text-[#CECE5A] text-[50px] font-bold underline mb-13">
+      <h1 className="text-center text-red-700 text-lg sm:text-2xl md:text-3xl lg:text-4xl lg:text-[#1A5D1A] text-[50px] font-bold underline mb-13">
         Hello world!
       </h1>
       <h1 className="text-3xl ">Hello you!</h1>
