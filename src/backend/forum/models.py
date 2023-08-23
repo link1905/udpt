b208a742b515512e6fe5d24138ca25c5ad83d9b9
Lâm Hoàng Phúc <64113128+TcMits@users.noreply.github.com>
2023-08-23 11:45:38 +0700

    fix(forum): allow large content

--- src/backend/forum/models.py
+++ src/backend/forum/models.py
@@ -62,7 +62,7 @@ class ThreadCategory(models.Model):
 
 class Thread(models.Model):
     title = models.CharField(max_length=255, default="", db_index=True)
-    content = models.TextField(db_index=True)
+    content = models.TextField()
     category = models.ForeignKey(ThreadCategory, on_delete=models.CASCADE, null=True, blank=True, related_name="threads")
 
     creator_id = models.PositiveIntegerField(db_index=True)
