9a3c6f2ecfe0880b5d38d6413ea107b3282cb028
Điền Tạ <63856613+blueackerman06@users.noreply.github.com>
2023-08-24 14:30:23 +0700

    Update code

--- src/frontend/my_forum(mergecode)/src/components/login-guard/login-guard.tsx
+++ src/frontend/my_forum(mergecode)/src/components/login-guard/login-guard.tsx
@@ -5,21 +5,30 @@ import {
   requestAuthRefresh,
 } from "../../services/account/auth-refresh.ts";
 import { LoadingOverlay } from "@mantine/core";
-import { Navigate } from "react-router-dom";
+import { Navigate, useMatch } from "react-router-dom";
 export function LoginGuard({ children }: PropsWithChildren<{}>) {
-  const { error, isLoading } = useQuery(
+  const { error, isLoading, data } = useQuery(
     authRequestQueryKey,
     requestAuthRefresh,
     {
       retry: 0,
     },
   );
+
+  const isAdminPage = useMatch({
+    path: "/admin",
+    end: false,
+  });
+
   if (isLoading) {
     return <LoadingOverlay visible />;
   }
   if (error) {
     return <Navigate to="/signin" />;
   }
-  
+  if (isAdminPage && !data?.fields?.is_staff) {
+    return <Navigate to="/" />;
+  }
+
   return <>{children}</>;
 }
--- src/frontend/my_forum(mergecode)/src/services/account/account.client.ts
+++ src/frontend/my_forum(mergecode)/src/services/account/account.client.ts
@@ -12,4 +12,5 @@ export interface AccountFields {
   first_name: string;
   last_name: string;
   username: string;
+  is_staff: boolean;
 }
